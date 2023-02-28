4c708a5be36dc887547289051c27ebf010b14fae
Alanjim Halabia <alanjim.halabia@hotmail.com>
2023-02-27 23:32:16 -0500

    feat: buttons with working scoreboard

--- javascript.js
+++ javascript.js
@@ -1,23 +1,22 @@
-RPS = () => {
+var x = 0;
+var y = 0;
+
+playRound = (decision) => {
 
 // play a 5 game round
-for (let i = 0; i < 5; i++) {
     const choices = ["rock", "paper", "scissors"]
     
     // player's decision
-    let decision = prompt("Please enter: 'rock','paper','scissors'").toLowerCase();;
-    console.log(decision)
+    // let decision = prompt("Please enter: 'rock','paper','scissors'").toLowerCase();;
+    // console.log(decision)
     
     // computer's decision
     let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
     console.log(getComputerChoice)
 
     // initialize counters to 0 for the first round
-    if (i == 0) {
-        var x = 0;
-        var y = 0;
-    }
-    else {
+
+    
         if (decision == "rock" & getComputerChoice == "rock" || decision == "scissors" & getComputerChoice == "scissors" || decision == "paper" & getComputerChoice == "paper") {
             console.log("Draw");
         } else if (decision == "rock" & getComputerChoice == "scissors" || decision == "paper" & getComputerChoice == "rock" || decision == "scissors" & getComputerChoice == "paper" ) {
@@ -29,17 +28,27 @@ for (let i = 0; i < 5; i++) {
             y++
             console.log("Their score is: ",y)
         }
-    }
-    }
-
-// decide if player wins or loses the game based on total round wins
-if (x>y) {
-        console.log("You win the game")
+    
 }
-if (x<y) {
-        console.log("You lose the game")
-}
-if (x==y) {
-        console.log("You draw the game")
-}
-}
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', function (e) {
+    playRound("rock")
+  });
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', function (e) {
+    playRound("scissors")
+});
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', function (e) {
+    playRound("paper")
+});
+// // decide if player wins or loses the game based on total round wins
+// if (x>y) {
+//         console.log("You win the game")
+// }
+// if (x<y) {
+//         console.log("You lose the game")
+// }
+// if (x==y) {
+//         console.log("You draw the game")
+// }
